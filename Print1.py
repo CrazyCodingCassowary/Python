65d4fbb08944673c9b76dc32ad3bf846cbaee77e
CrazyCodingCassowary <suttisom.nattika@gmail.com>
2025-07-31 13:09:38 +0700

    Add files via upload

---
+++ Print1.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("Welcome to the world of programming.")
+#print a number
+print(4)
+#next line after printing
+print("Hello Python \n")
+
+#pass multiple arguments for printing
+print("hello",5)
+#end argument for print statement
