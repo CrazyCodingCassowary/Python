9d461f4860d9c05855eec342d4fd9d5d90f23699
CrazyCodingCassowary <suttisom.nattika@gmail.com>
2025-08-12 13:04:23 +0700

    Add files via upload

--- Operators.py
+++ Operators.py
@@ -8,3 +8,18 @@ sum=tree1+tree2+tree3+tree4+tree5
 
 average=sum/5
 print("average of all trees is: ", average)
+
+
+Amount=int(input("Please Enter Amount To Withdraw"))
+
+note_1=Amount//100
+note_2=(Amount%100)//50
+note_3=((Amount%100)%50)//10
+
+print("notes of 100 rupees", note_1)
+print("notes of 50 rupees", note_2)
+print("notes of 10 rupees", note_3)
+
+
+
+
