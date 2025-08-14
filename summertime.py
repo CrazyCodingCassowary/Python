e9ee65fa8007adaf4f07e2d7f408eaf69e68f9ee
CrazyCodingCassowary <suttisom.nattika@gmail.com>
2025-08-13 23:35:24 -0700

    Add files via upload

---
+++ summertime.py
@@ -0,0 +1,12 @@
+i=int(input("enter the temperature: "))
+
+if (i<25):
+    print("it's too cold! Bring warm clothes.")
+   
+
+else:
+     print("Its's warm and sunny! Leave your jacket at home.")
+     
+
+
+
