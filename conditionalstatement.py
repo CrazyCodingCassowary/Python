7b21b717623843ae4d214ccc593743f901336820
CrazyCodingCassowary <suttisom.nattika@gmail.com>
2025-08-14 13:21:12 +0700

    Add files via upload

---
+++ conditionalstatement.py
@@ -0,0 +1,37 @@
+num=-1
+if num>0:
+    print(num, "is a positive number")
+else:
+    print(num, "is a negative number")
+
+num=3
+if num>0:
+    print(num, "is a posiTtive number")
+
+actual_cost=float(input("Please enter the Actual Product Price"))
+sale_amount=float(input("Please enter the Sales Amount:"))
+
+if(sale_amount>actual_cost):
+    amount=sale_amount-actual_cost
+    print("Total Profit={0}".format(amount))
+else:
+    print("No Profit")
+
+i=int(input("enter a number: "))
+
+if (i<15):
+    print("i is smaller than 15")
+    print("this is the [if] block")
+
+else:
+     print("i is greater than 15")
+     print("this is the [else] block")
+print("This is not it [if], nor is it in [else]")
+
+number=int(input("Enter a number to check whether it is even or odd."))
+print("\n Your number of choice is",number)
+
+if number%2==0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
