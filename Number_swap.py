d8a8cf29de3638f0e5995bd2d72ebd76bf47ef18
CrazyCodingCassowary <suttisom.nattika@gmail.com>
2025-08-25 23:36:07 -0700

    Add files via upload

---
+++ Number_swap.py
@@ -0,0 +1,8 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+num1, num2, num3 = num3, num1, num2
+
+print ("num1 = ",num1, "num2 = ",num2, "num3 =", num3)
